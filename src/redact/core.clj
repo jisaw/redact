3fce385c39887f02b619e44add9d55d311b0ebf9
Jacob, Sawyer <sawyer.jake@gmail.com>
2016-07-26 21:09:41 -0500

    init clojure app

---
+++ src/redact/core.clj
@@ -0,0 +1,6 @@
+(ns redact.core)
+
+(defn foo
+  "I don't do a whole lot."
+  [x]
+  (println x "Hello, World!"))
---
+++ test/redact/core_test.clj
@@ -0,0 +1,7 @@
+(ns redact.core-test
+  (:require [clojure.test :refer :all]
+            [redact.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
