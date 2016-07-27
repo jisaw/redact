f484225e31df18270b3117f2654d5c8344a2e8dc
Jacob, Sawyer <sawyer.jake@gmail.com>
2016-07-26 22:55:25 -0500

    created function for parsing CSV and sorting command line args

--- src/redact/core.clj
+++ src/redact/core.clj
@@ -1,6 +1,28 @@
-(ns redact.core)
+(ns redact.core
+  (:gen-class)
+  (:require [clojure.java.io :as io])
+  (:require [clojure.string :as str]))
 
-(defn foo
-  "I don't do a whole lot."
-  [x]
-  (println x "Hello, World!"))
+(defn -main
+  [& args]
+  (def stoplist [])
+  (map #(dispatch %) args))
+
+(defn read-csv
+  ;; Takes in a filename and returns a vector of the csv values
+  [file-name]
+  (str/split (with-open [rdr (io/reader file-name)]
+     (doall (reduce str (line-seq rdr)))) #","))
+
+
+(defn dispatch
+  ;; Checks if the object is this or that and sends it to the right place
+  [element]
+  (if (.contains element ".txt")
+    (def text-file element))
+  (if (.contains element ".csv")
+    (try 
+      (def stoplist (distinct (concat stoplist (read-csv element))))
+      (catch Exception e (println "Exception caught: " (.getMessage e))))
+    (def stoplist (distinct (conj stoplist element))))
+  )
