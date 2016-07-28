fb5423b1c766428c1287f3d7a5eae10630de25d3
Jacob, Sawyer <sawyer.jake@gmail.com>
2016-07-27 22:23:09 -0500

    Created a working redactor!

--- src/redact/core.clj
+++ src/redact/core.clj
@@ -1,12 +1,28 @@
 (ns redact.core
   (:gen-class)
-  (:require [clojure.java.io :as io])
-  (:require [clojure.string :as str]))
+  (:require [clojure.java.io :as io]
+            [clojure.string :as str]
+))
 
-(defn -main
-  [& args]
-  (def stoplist [])
-  (map #(dispatch %) args))
+(defn redact-doc
+  ;; Reads the file line by line and redacts all the matched words
+  ([target stoplist]
+   (if (empty? stoplist)
+     (println target)
+     (redact-doc (str/replace target (re-pattern (first stoplist)) "REDACTED") (rest stoplist))))
+  )
+
+(defn get-target-text
+  ;; Takes a vector of args and returns a String of a text file
+  ([args]
+   (if (empty? args)
+     (println "No text file was found!")
+     (if (.contains (first args) ".txt")
+       (str (slurp (first args)))
+       (get-target-text (rest args)))))
+  ;; TODO: Change to regex match for mutliple text based file types
+  
+ )
 
 (defn read-csv
   ;; Takes in a filename and returns a vector of the csv values
@@ -14,15 +30,19 @@
   (str/split (with-open [rdr (io/reader file-name)]
      (doall (reduce str (line-seq rdr)))) #","))
 
+(defn gen-stoplist
+  ;; Generates the stoplist for words to be redacted
+  ([args] (gen-stoplist args []))
+  ([args stoplist]
+   (if (empty? args)
+     stoplist
+     (gen-stoplist (rest args) (if (.contains (first args) ".csv")
+                                 (into [] (concat stoplist (read-csv (first args))))
+                                 (if (.contains (first args) ".txt")
+                                   stoplist
+                                   (into [] (concat stoplist [(first args)] ))))))))
 
-(defn dispatch
-  ;; Checks if the object is this or that and sends it to the right place
-  [element]
-  (if (.contains element ".txt")
-    (def text-file element))
-  (if (.contains element ".csv")
-    (try 
-      (def stoplist (distinct (concat stoplist (read-csv element))))
-      (catch Exception e (println "Exception caught: " (.getMessage e))))
-    (def stoplist (distinct (conj stoplist element))))
-  )
+(defn -main
+  [& args]
+  (redact-doc (get-target-text args) (gen-stoplist args))
+)
